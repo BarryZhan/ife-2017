6f35bf5ef69411eaffc9905e524675c70ed2e23e
湛礼彬 <11912346@qq.com>
2017-03-15 18:34:34 +0800

    Update vue task2

--- vue/task2/index.js
+++ vue/task2/index.js
@@ -1,34 +1,29 @@
-function PubSub() {
-    this.handlers = {}
-}
-
-PubSub.prototype = {
-    on (handlerType, handler) {
-        if (!this.handlers.hasOwnProperty(handlerType)) {
-            this.handlers[handlerType] = []
-        }
-        this.handlers[handlerType].push(handler)
-    },
-    emit (handlerType) {
-        let args = Array.prototype.slice.call(arguments, 1)
-        this.handlers[handlerType] && this.handlers[handlerType].forEach((handler) => {
-            handler(...args)
-        })
-    }
-}
-
-
 function Observer(data) {
     this.data = data
-    this.pubsub = new PubSub()
+    // 事件队列
+    this.handlers = {}
     this.walk(data)
+}
+
+
+Observer.prototype.emit = function (handlerType) {
+    let args = Array.prototype.slice.call(arguments, 1)
+    this.handlers[handlerType] && this.handlers[handlerType].forEach((handler) => {
+        handler(...args)
+    })
+}
+
+Observer.prototype.on = function (handlerType, handler) {
+    if (!this.handlers[handlerType]) {
+        this.handlers[handlerType] = []
+    }
+    this.handlers[handlerType].push(handler)
 }
 
 Observer.prototype.walk = function (obj) {
     // 遍历出所有可枚举的key
     Object.keys(obj).forEach(key => {
         let val = obj[key]
-        this.pubsub.emit(key, val)
 
         // 如果值是个对象，就递归它，get/set
         if (typeof val === 'object') {
@@ -50,7 +45,7 @@ Observer.prototype.convert = function (key, val) {
         },
         set (newVal) {
             console.log(`你设置了${key}，新的值为${newVal}`)
-            that.pubsub.emit(key, val, newVal)
+            that.emit(key, newVal, val)
             val = newVal
             // 如果新赋的值是个对象，就递归他，加上get/set
             if (typeof val === 'object') {
@@ -61,7 +56,7 @@ Observer.prototype.convert = function (key, val) {
 }
 
 Observer.prototype.$watch = function (arrt, callback) {
-    this.pubsub.on(arrt, callback)
+    this.on(arrt, callback)
 }
 
 
@@ -83,7 +78,7 @@ app1.data.name.lastName
 app1.data.name.firstName = 'lalala'
 
 // 实现 $watch 这个 API
-app1.$watch('age', function (val, newVal) {
+app1.$watch('age', function (newVal, val) {
     console.log(`我的年纪变了，从${newVal}变到了${val}`)
 })
 
